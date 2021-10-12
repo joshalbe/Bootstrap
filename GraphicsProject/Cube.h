085763638fd96702242d1afbd17c4b30eb21bfc2
Josh Albe <joshjalbe@gmail.com>
2021-10-12 13:09:31 -0500

    Created DragonWorld and began creating Cube

---
+++ GraphicsProject/Cube.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Mesh.h"
+
+class Cube : public Mesh
+{
+public:
+	Cube() {}
+	~Cube() {}
+
+	Vertex* generateVertices(unsigned int& vertexCount, unsigned int& triCount) override;
+
+	glm::vec4 getColor() { return m_color; }
+	void setColor(glm::vec4 color) { m_color = color; }
+
+private:
+	glm::vec4 m_color = glm::vec4(1.0f);
+
+private:
+	void setVertexPosition(Vertex* vertices, int vertexCount, int index, glm::vec3 position);
+
+};
+
