52c4c17399e40fd9dadb685b1eeed45b7f3ed11a
Josh Albe <joshjalbe@gmail.com>
2021-10-14 13:00:45 -0500

    Removed unnecessary comments

--- GraphicsProject/Cube.cpp
+++ GraphicsProject/Cube.cpp
@@ -9,73 +9,57 @@ Cube::Vertex* Cube::generateVertices(unsigned int& vertexCount, unsigned int& tr
 	vertices = new Vertex[vertexCount];
 
 	/*
-	initializeCorner(vertices, vertexCount, 5, { -1.0f,		 1.0f,		-1.0f,  });  //A6
-	initializeCorner(vertices, vertexCount, 2, {  1.0f,		 1.0f,		 1.0f,  });  //B0
-	initializeCorner(vertices, vertexCount, 7, {  1.0f,		-1.0f,		-1.0f,  });  //C4
-	initializeCorner(vertices, vertexCount, 0, { -1.0f,		-1.0f,		 1.0f,  });  //D2
-	initializeCorner(vertices, vertexCount, 6, {  1.0f,		 1.0f,		-1.0f,  });  //E5
-	initializeCorner(vertices, vertexCount, 1, { -1.0f,		 1.0f,		 1.0f,  });  //F1
-	initializeCorner(vertices, vertexCount, 4, { -1.0f,		-1.0f,		-1.0f,  });  //G7
-	initializeCorner(vertices, vertexCount, 3, {  1.0f,		-1.0f,		 1.0f,  });  //H3
-	*/
-
-
-	//Sequence AEBFGCHD?
-	//Missing 4 triangles, and the existing ones overlap
-
-	/*
 		AFBEGDHC
 
 		A---E		6---5		G---C		7---4
 		|	|	->	|	|		|	|	->	|	|
 		F---B		1---0		D---H		2---3
 
-		Failures:
 
 	*/
-
+	
 	//Front Face 012-230
-	initializeCorner(vertices, vertexCount, 0, { 1.0f,		 1.0f,		 1.0f, });  //B0
-	initializeCorner(vertices, vertexCount, 1, { -1.0f,		 1.0f,		 1.0f, });  //F1
-	initializeCorner(vertices, vertexCount, 2, { -1.0f,		-1.0f,		 1.0f, });  //D2
+	initializeCorner(vertices, vertexCount, 0,  {  1.0f,		 1.0f,		 1.0f, });  //B0
+	initializeCorner(vertices, vertexCount, 1,  { -1.0f,		 1.0f,		 1.0f, });  //F1
+	initializeCorner(vertices, vertexCount, 2,  { -1.0f,		-1.0f,		 1.0f, });  //D2
 
-	initializeCorner(vertices, vertexCount, 3, { -1.0f,		-1.0f,		 1.0f, });  //D2
-	initializeCorner(vertices, vertexCount, 4, { 1.0f,		-1.0f,		 1.0f, });  //H3
-	initializeCorner(vertices, vertexCount, 5, { 1.0f,		 1.0f,		 1.0f, });  //B0
+	initializeCorner(vertices, vertexCount, 3,  { -1.0f,		-1.0f,		 1.0f, });  //D2
+	initializeCorner(vertices, vertexCount, 4,  {  1.0f,		-1.0f,		 1.0f, });  //H3
+	initializeCorner(vertices, vertexCount, 5,  {  1.0f,		 1.0f,		 1.0f, });  //B0
 
 	//Right Face 034-450
-	initializeCorner(vertices, vertexCount, 6, { 1.0f,		 1.0f,		 1.0f, });  //B0
-	initializeCorner(vertices, vertexCount, 7, { 1.0f,		-1.0f,		 1.0f, });  //H3
-	initializeCorner(vertices, vertexCount, 8, { 1.0f,		-1.0f,		-1.0f, });  //C4
+	initializeCorner(vertices, vertexCount, 6,  {  1.0f,		 1.0f,		 1.0f, });  //B0
+	initializeCorner(vertices, vertexCount, 7,  {  1.0f,		-1.0f,		 1.0f, });  //H3
+	initializeCorner(vertices, vertexCount, 8,  {  1.0f,		-1.0f,		-1.0f, });  //C4
 
-	initializeCorner(vertices, vertexCount, 9, { 1.0f,		-1.0f,		-1.0f, });  //C4
-	initializeCorner(vertices, vertexCount, 10, { 1.0f,		 1.0f,		-1.0f, });  //E5
-	initializeCorner(vertices, vertexCount, 11, { 1.0f,		 1.0f,		 1.0f, });  //B0
+	initializeCorner(vertices, vertexCount, 9,  {  1.0f,		-1.0f,		-1.0f, });  //C4
+	initializeCorner(vertices, vertexCount, 10, {  1.0f,		 1.0f,		-1.0f, });  //E5
+	initializeCorner(vertices, vertexCount, 11, {  1.0f,		 1.0f,		 1.0f, });  //B0
 
 	//Top Face 056-610
-	initializeCorner(vertices, vertexCount, 12, { 1.0f,		 1.0f,		 1.0f, });  //B0
-	initializeCorner(vertices, vertexCount, 13, { 1.0f,		 1.0f,		-1.0f, });  //E5
+	initializeCorner(vertices, vertexCount, 12, {  1.0f,		 1.0f,		 1.0f, });  //B0
+	initializeCorner(vertices, vertexCount, 13, {  1.0f,		 1.0f,		-1.0f, });  //E5
 	initializeCorner(vertices, vertexCount, 14, { -1.0f,		 1.0f,		-1.0f, });  //A6
 
 	initializeCorner(vertices, vertexCount, 15, { -1.0f,		 1.0f,		-1.0f, });  //A6
 	initializeCorner(vertices, vertexCount, 16, { -1.0f,		 1.0f,		 1.0f, });  //F1
-	initializeCorner(vertices, vertexCount, 17, { 1.0f,		 1.0f,		 1.0f, });  //B0
+	initializeCorner(vertices, vertexCount, 17, {  1.0f,		 1.0f,		 1.0f, });  //B0
 
 	//Back Face 765-547
 	initializeCorner(vertices, vertexCount, 18, { -1.0f,		-1.0f,		-1.0f, });  //G7
 	initializeCorner(vertices, vertexCount, 19, { -1.0f,		 1.0f,		-1.0f, });  //A6
-	initializeCorner(vertices, vertexCount, 20, { 1.0f,		 1.0f,		-1.0f, });  //E5
+	initializeCorner(vertices, vertexCount, 20, {  1.0f,		 1.0f,		-1.0f, });  //E5
 
-	initializeCorner(vertices, vertexCount, 21, { 1.0f,		 1.0f,		-1.0f, });  //E5
-	initializeCorner(vertices, vertexCount, 22, { 1.0f,		-1.0f,		-1.0f, });  //C4
+	initializeCorner(vertices, vertexCount, 21, {  1.0f,		 1.0f,		-1.0f, });  //E5
+	initializeCorner(vertices, vertexCount, 22, {  1.0f,		-1.0f,		-1.0f, });  //C4
 	initializeCorner(vertices, vertexCount, 23, { -1.0f,		-1.0f,		-1.0f, });  //G7
 
 	//Bottom Face 743-327
 	initializeCorner(vertices, vertexCount, 24, { -1.0f,		-1.0f,		-1.0f, });  //G7
-	initializeCorner(vertices, vertexCount, 25, { 1.0f,		-1.0f,		-1.0f, });  //C4
-	initializeCorner(vertices, vertexCount, 26, { 1.0f,		-1.0f,		 1.0f, });  //H3
+	initializeCorner(vertices, vertexCount, 25, {  1.0f,		-1.0f,		-1.0f, });  //C4
+	initializeCorner(vertices, vertexCount, 26, {  1.0f,		-1.0f,		 1.0f, });  //H3
 
-	initializeCorner(vertices, vertexCount, 27, { 1.0f,		-1.0f,		 1.0f, });  //H3
+	initializeCorner(vertices, vertexCount, 27, {  1.0f,		-1.0f,		 1.0f, });  //H3
 	initializeCorner(vertices, vertexCount, 28, { -1.0f,		-1.0f,		 1.0f, });  //D2
 	initializeCorner(vertices, vertexCount, 29, { -1.0f,		-1.0f,		-1.0f, });  //G7
 
@@ -87,7 +71,7 @@ Cube::Vertex* Cube::generateVertices(unsigned int& vertexCount, unsigned int& tr
 	initializeCorner(vertices, vertexCount, 33, { -1.0f,		 1.0f,		 1.0f, });  //F1
 	initializeCorner(vertices, vertexCount, 34, { -1.0f,		 1.0f,		-1.0f, });  //A6
 	initializeCorner(vertices, vertexCount, 35, { -1.0f,		-1.0f,		-1.0f, });  //G7
-
+	
 
 	return vertices;
 }
